c1bdccf7e66a5cfce3f84dc29bbe4ca094796a19
manohySr <manohyrajaonah@gmail.com>
2024-11-08 15:47:19 +0400

    add:   configuration for style

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,14 @@
-<template>Hello world</template>
+<script setup>
+import image from "@/assets/images/image-cake-desktop.jpg";
+</script>
+<template>
+  <div class="container">
+    <img :src="image" alt="cake" />
+    <button>Add to Cart</button>
+  </div>
+  <div class="description">
+    <span>Cake</span>
+    <span>Red Velvet Cake</span>
+    <span>$4.50</span>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,22 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["sans-serif"],
+        sans: ["RedHatText", "sans-serif"],
       },
       gridTemplateColumns: {
         "70/30": "70% 28%",
+      },
+      colors: {
+        red: "hsl(14, 86%, 42%)",
+        green: "hsl(159, 69%, 38%)",
+        rose: {
+          50: "hsl(20, 50%, 98%)",
+          100: "hsl(13, 31%, 94%)",
+          300: "hsl(14, 25%, 72%)",
+          400: "hsl(7, 20%, 60%)",
+          500: "hsl(12, 20%, 44%)",
+          900: "hsl(14, 65%, 9%)",
+        },
       },
     },
   },
