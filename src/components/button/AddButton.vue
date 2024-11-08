ba1b44a1fcbceba2e171694ffbdfc5a827c4e80a
manohySr <manohyrajaonah@gmail.com>
2024-11-08 18:53:21 +0400

    add:    add button for cart
    the add to cart button

---
+++ src/components/button/AddButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+import cart from "@/assets/images/icon-add-to-cart.svg";
+</script>
+<template>
+  <button
+    class="bg-white w-56 flex items-center gap-2 justify-center text-black px-5 border-2 border-rose-300 rounded-full"
+    style="padding-top: 0.9rem; padding-bottom: 0.9rem"
+  >
+    <img :src="cart" alt="Cart Icon" />
+    Add to Cart
+  </button>
+</template>
