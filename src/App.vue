17153055da8fb3764a8b5c9dc8216e4bbc9c9d40
manohySr <manohyrajaonah@gmail.com>
2024-11-08 18:54:35 +0400

    add:   layout
    starting to make style in it

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup>
-import image from "@/assets/images/image-cake-desktop.jpg";
+import image from "@/assets/images/image-cake-mobile.jpg";
+import CartButton from "./components/button/CartButton.vue";
+import AddButton from "./components/button/AddButton.vue";
 </script>
 <template>
-  <div class="container">
-    <img :src="image" alt="cake" />
-    <button>Add to Cart</button>
-  </div>
-  <div class="description">
-    <span>Cake</span>
-    <span>Red Velvet Cake</span>
-    <span>$4.50</span>
+  <div class="container p-5">
+    <h1 class="text-3xl font-bold mb-5">Desserts</h1>
+    <div class="relative flex flex-col items-center">
+      <img :src="image" class="w-full object-cover rounded-lg" alt="cake" />
+      <div class="button">
+        <AddButton />
+        <!-- <CartButton /> -->
+      </div>
+    </div>
+    <div class="mt-10">
+      <div class="description flex flex-col">
+        <span class="text-rose-500">Cake</span>
+        <span class="font-semibold">Red Velvet Cake</span>
+        <span class="text-red font-semibold">$4.50</span>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.button {
+  position: absolute;
+  bottom: -2rem;
+  z-index: 1000 !important;
+}
+</style>
