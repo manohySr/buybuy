9fe2e958d26826a428f45a7829d3d5e3cc2413bc
manohySr <manohyrajaonah@gmail.com>
2024-11-09 21:53:56 +0400

    add:    assests in public repository so we can use it as static file

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <script setup>
-import image from "@/assets/images/image-cake-mobile.jpg";
-import CartButton from "./components/button/CartButton.vue";
-import AddButton from "./components/button/AddButton.vue";
+import CartButton from "@/components/button/CartButton.vue";
+import AddButton from "@/components/button/AddButton.vue";
+import { desserts as _desserts } from "./data";
+import { reactive } from "vue";
 
-const items = [1, 2, 3, 1, 5, 2];
+function computeReplaceImage(items) {
+  const updatedItems = items.map((item) => {
+    // Iterate over each image key and replace './assets/' with ''
+    const updatedImages = Object.fromEntries(
+      Object.entries(item.image).map(([key, value]) => [
+        key,
+        value.replace("./assets/", ""),
+      ]),
+    );
+
+    return reactive({
+      ...item,
+      image: updatedImages,
+      amount: 0,
+      cartState: false,
+    });
+  });
+
+  return updatedItems;
+}
+
+const desserts = reactive(computeReplaceImage(_desserts));
+
+const increment = (dessert) => {
+  dessert.amount++;
+};
+
+const decrement = (dessert) => {
+  if (dessert.amount == 1) {
+    dessert.cartState = false;
+  }
+  dessert.amount--;
+};
+
+const handleClick = (dessert) => {
+  increment(dessert);
+  dessert.cartState = true;
+};
 </script>
 <template>
   <div class="container p-5">
     <h1 class="text-3xl font-bold mb-5">Desserts</h1>
-    <div class="dessert" v-for="item of items">
+    <div class="dessert" v-for="dessert in desserts" :key="dessert.name">
       <div class="relative flex flex-col items-center">
-        <img :src="image" class="w-full object-cover rounded-lg" alt="cake" />
+        <img
+          :src="dessert.image.mobile"
+          class="w-full object-cover rounded-lg"
+          alt="cake"
+        />
         <div class="button">
-          <AddButton />
-          <!-- <CartButton /> -->
+          <AddButton
+            v-if="!dessert.cartState"
+            @click="() => handleClick(dessert)"
+          />
+          <CartButton
+            v-else
+            :amount="dessert.amount"
+            :increment="() => increment(dessert)"
+            :decrement="() => decrement(dessert)"
+          />
         </div>
       </div>
       <div class="mt-10">
         <div class="description flex flex-col">
-          <span class="text-rose-500">Cake</span>
-          <span class="font-semibold">Red Velvet Cake</span>
-          <span class="text-red font-semibold">$4.50</span>
+          <span class="text-rose-500">{{ dessert.category }}</span>
+          <span class="font-semibold">{{ dessert.name }}</span>
+          <span class="text-red font-semibold">${{ dessert.price }}</span>
         </div>
       </div>
     </div>
