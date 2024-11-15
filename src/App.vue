bdf8b482d58b5bae25eb4f80f93cb7ac2f73f9f0
manohySr <manohyrajaonah@gmail.com>
2024-11-15 19:18:36 +0400

    wip:    responsive

--- src/App.vue
+++ src/App.vue
@@ -51,31 +51,40 @@ const handleClick = (dessert) => {
 <template>
   <div class="container bg-rose-50 p-5">
     <h1 class="text-3xl font-bold mb-5">Desserts</h1>
-    <div class="dessert" v-for="dessert in desserts" :key="dessert.name">
-      <div class="relative flex flex-col items-center">
-        <img
-          :src="dessert.image.mobile"
-          class="w-full object-cover rounded-lg"
-          alt="cake"
-        />
-        <div class="button">
-          <AddButton
-            v-if="!dessert.cartState"
-            @click="() => handleClick(dessert)"
-          />
-          <CartButton
-            v-else
-            :amount="dessert.amount"
-            @increment="() => increment(dessert)"
-            @decrement="() => decrement(dessert)"
-          />
+    <div class="dessert">
+      <div class="mt" v-for="dessert in desserts" :key="dessert.name">
+        <div class="relative flex flex-col items-center">
+          <picture class="w-full">
+            <source
+              :srcset="dessert.image.desktop"
+              media="(min-width: 1440px)"
+            />
+            <source :srcset="dessert.image.tablet" media="(min-width: 768px)" />
+            <img
+              :src="dessert.image.mobile"
+              class="rounded-lg w-full object-cover"
+              alt="cake"
+            />
+          </picture>
+          <div class="button">
+            <AddButton
+              v-if="!dessert.cartState"
+              @click="() => handleClick(dessert)"
+            />
+            <CartButton
+              v-else
+              :amount="dessert.amount"
+              @increment="() => increment(dessert)"
+              @decrement="() => decrement(dessert)"
+            />
+          </div>
         </div>
-      </div>
-      <div class="mt-10">
-        <div class="description flex flex-col">
-          <span class="text-rose-500">{{ dessert.category }}</span>
-          <span class="font-semibold">{{ dessert.name }}</span>
-          <span class="text-red font-semibold">${{ dessert.price }}</span>
+        <div class="mt-10">
+          <div class="description flex flex-col">
+            <span class="text-rose-500">{{ dessert.category }}</span>
+            <span class="font-semibold">{{ dessert.name }}</span>
+            <span class="text-red font-semibold">${{ dessert.price }}</span>
+          </div>
         </div>
       </div>
     </div>
@@ -84,13 +93,32 @@ const handleClick = (dessert) => {
 </template>
 
 <style scoped>
+.container {
+  margin: 0 auto;
+}
+
 .button {
   position: absolute;
   bottom: -2rem;
   z-index: 1000 !important;
 }
 
-.dessert {
+.mt {
   margin-bottom: 2rem;
+}
+
+.dessert {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  padding: 16px;
+}
+@media (max-width: 768px) {
+  .dessert {
+    display: flex;
+    flex-direction: column; /* Empile les éléments */
+    gap: 16px; /* Espacement vertical entre les éléments */
+    padding: 16px;
+  }
 }
 </style>
